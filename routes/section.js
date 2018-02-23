e08734144ed645ff33a4386d9ccb676d29ae2911
svaiyapuri@apple.com <svaiyapuri@apple.com>
2018-02-23 11:57:17 +0530

    Integrate Server changes

---
+++ routes/section.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send('Express RESTful API');
+});
+
+router.post('/', function(req, res, next) {
+    res.send('Express RESTful API');
+});
+
+module.exports = router;
